d609f8f106ede83d24c1369a2e069f4630443c46
Zebrusha <146824554+Zebrusha@users.noreply.github.com>
2023-11-28 15:25:32 +0300

    change src

--- src/main/java/ru/netology/statistic/StatisticService.java
+++ src/main/java/ru/netology/statistic/StatisticService.java
@@ -4,7 +4,7 @@ public class StatisticService {
     public long findMax(long[] incomes) {
         long currentMax = incomes[0];
         for (long income : incomes) {
-            if (currentMax < income) {
+            if (currentMax <= income) {
                 currentMax = income;
             }
         }
